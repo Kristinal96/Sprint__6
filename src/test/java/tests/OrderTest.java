9856cbbd3e9c1f0c0107269854f9e578fb62fd14
Kristina <Kristinal9678@yandex.com>
2025-09-28 23:25:43 +0600

    commit2

--- src/test/java/tests/OrderTest.java
+++ src/test/java/tests/OrderTest.java
@@ -14,12 +14,12 @@ import pages.OrderPage2;
 import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class OrderTest2 {
+public class OrderTest {
 
     private WebDriver driver;
-    private pages.MainPage mainPage;
-    private pages.OrderPage1 orderPage1;
-    private pages.OrderPage2 orderPage2;
+    private MainPage mainPage;
+    private OrderPage1 orderPage1;
+    private OrderPage2 orderPage2;
 
     @BeforeEach
     void setUp() {
@@ -27,17 +27,23 @@ public class OrderTest2 {
         options.addArguments("--no-sandbox", "--disable-dev-shm-usage");
         driver = new ChromeDriver(options);
         driver.get("https://qa-scooter.praktikum-services.ru/");
-        mainPage = new pages.MainPage(driver);
-        orderPage1 = new pages.OrderPage1(driver);
-        orderPage2 = new pages.OrderPage2(driver);
+        mainPage = new MainPage(driver);
+        orderPage1 = new OrderPage1(driver);
+        orderPage2 = new OrderPage2(driver);
         mainPage.closeCookieConsent(); // Закрываем всплывающее окно с согласием на использование файлов cookie
     }
 
     @ParameterizedTest
     @MethodSource("orderDataProvider")
-    void testOrderFlow(String name, String surname, String address, String metro, String phone, String date, String rentalPeriod, String color, String comment) {
-        // Нажать кнопку «Заказать» внизу страницы
-        mainPage.clickOrderButtonBottom();
+    void testOrderFlow(String name, String surname, String address, String metro, String phone, String date, String rentalPeriod, String color, String comment, String buttonType) {
+        // Нажать кнопку «Заказать» (верхнюю или нижнюю)
+        if ("top".equals(buttonType)) {
+            mainPage.clickOrderButtonTop();
+        } else if ("bottom".equals(buttonType)) {
+            mainPage.clickOrderButtonBottom();
+        } else {
+            throw new IllegalArgumentException("Invalid button type: " + buttonType);
+        }
 
         // Заполнить форму заказа
         orderPage1.setName(name);
@@ -62,8 +68,8 @@ public class OrderTest2 {
 
     static Stream<Arguments> orderDataProvider() {
         return Stream.of(
-                Arguments.of("Иван", "Иванов", "Москва, ул. Ленина, 1", "Бульвар Рокоссовского", "+79991234567", "15.10.2023", "сутки", "grey", "Пожалуйста, позвоните заранее"),
-                Arguments.of("Петр", "Петров", "Санкт-Петербург, ул. Пушкина, 2", "Бульвар Рокоссовского", "+79997654321", "16.04.2024", "сутки", "grey", "Оставьте у двери")
+                Arguments.of("Иван", "Иванов", "Москва, ул. Ленина, 1", "Бульвар Рокоссовского", "+79991234567", "15.10.2023", "сутки", "grey", "Пожалуйста, позвоните заранее", "top"),
+                Arguments.of("Петр", "Петров", "Санкт-Петербург, ул. Пушкина, 2", "Бульвар Рокоссовского", "+79997654321", "16.04.2024", "сутки", "grey", "Оставьте у двери", "bottom")
         );
     }
 
